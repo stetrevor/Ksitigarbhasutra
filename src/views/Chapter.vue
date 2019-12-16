f83341f7b3da201791a8a94cd3bad5b4edea1e14
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-12-16 12:38:09 +0800

    Update chapter navigation

--- src/views/Chapter.vue
+++ src/views/Chapter.vue
@@ -5,7 +5,7 @@
         >上一章</router-link
       >
       <router-link to="/">目录</router-link>
-      <router-link :to="`/chapter/${chapterNum + 1}`" v-if="chapterNum < 28"
+      <router-link :to="`/chapter/${chapterNum + 1}`" v-if="chapterNum < 13"
         >下一章</router-link
       >
     </div>
@@ -17,7 +17,7 @@
         >上一章</router-link
       >
       <router-link to="/">目录</router-link>
-      <router-link :to="`/chapter/${chapterNum + 1}`" v-if="chapterNum < 28"
+      <router-link :to="`/chapter/${chapterNum + 1}`" v-if="chapterNum < 13"
         >下一章</router-link
       >
     </div>
